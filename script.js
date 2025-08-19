90609ea3af433de40d410d00fb23956fec3f9a5e
philipe <philipedarosa10@gmail.com>
2025-08-18 23:22:47 -0300

    pronto

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let container = document.querySelector('.container')
 let itens = container.querySelectorAll('.list .item')
 let indicadores = document.querySelector('.indicadores')
 let dots = indicadores.querySelectorAll("ul li")
+let list = container.querySelector('.list')
 
 
 let active = 0
@@ -11,21 +12,41 @@ let firstPosition = 0
 let lastPosition = itens.length - 1
 
 
-proxButton.onclick = () => {
-    let itemOld = container.querySelector('.list .item.active')
+function setSlider(){
+
+
+
+let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
 
+      let dotsOld = indicadores.querySelector('ul li.ativo')
+    dotsOld.classList.remove('ativo')
+    dots[active].classList.add('ativo')
+
+    indicadores.querySelector('.numeros').innerHTML = '0' +  (active + 1)
+
+}
+
+
+
+proxButton.onclick = () => {
+    
+    list.style.setProperty('--calculation', 1)
+    
     active = active + 1 > lastPosition ? 0 : active + 1
+    setSlider()
     itens[active].classList.add('active')
 
 
 }
 
 antButton.onclick = () => {
-    let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
+    list.style.setProperty('--calculation', -1)
 
     active = active - 1 < firstPosition ? lastPosition : active - 1
+    setSlider()
     itens[active].classList.add('active')
+    
+
 
 }
